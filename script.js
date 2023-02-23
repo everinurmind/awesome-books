59039a88013bfec51d662cbdb1d0142d755cc733
everinurmind <nurbol.sultanov@gmail.com>
2023-02-23 17:33:43 -0600

    added js for section display

--- script.js
+++ script.js
@@ -55,3 +55,29 @@ class BookApp {
 
 const bookApp = new BookApp();
 bookApp.displayBooks();
+
+// Sections switch
+
+const links = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('main section');
+
+for (const link of links) {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').slice(1);
+    for (const section of sections) {
+      if (section.id === targetId) {
+        section.classList.add('active');
+      } else {
+        section.classList.remove('active');
+      }
+    }
+    for (const link of links) {
+      if (link === event.target) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    }
+  });
+}
